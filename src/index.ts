d1bb031498e4d7793f48bac41989857ea79424c6
Franco de Desarrollo <francoegi01@gmail.com>
2023-10-17 20:36:41 -0300

    feat: add paths and modularice app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import express from 'express'
-const app = express()
-const { PORT = 3000 } = process.env
+import app from '@/app'
 
-app.get('/', (_req, res) => {
-  res.json({ message: 'Hello World!' })
-})
+const { PORT = 3000 } = process.env
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
