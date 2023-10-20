6e7ac4aabcba7db2ee88e64509dfe963559671a4
Franco de Desarrollo <francoegi01@gmail.com>
2023-10-20 20:43:40 -0300

    refact: modularice logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express, { type ErrorRequestHandler } from 'express'
-import mainRouter from './routes'
+import mainRouter from '@/routes'
+import { logger } from '@/middleware/logger'
 const app = express()
 
 app.use(express.json())
-app.use((req, _res, next) => {
-  console.log('\x1b[36m%s\x1b[0m', `${req.method} ${req.path}`)
-  next()
-})
+app.use(logger)
 
 app.use(mainRouter)
 app.use(((err, _req, res, _next) => {
