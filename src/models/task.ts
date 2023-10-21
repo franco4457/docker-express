6815302f06a8a2762118b93427c860208081e46d
Franco de Desarrollo <francoegi01@gmail.com>
2023-10-21 15:23:22 -0300

    refact: task model

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,10 +1,10 @@
+import { type ITask } from '@/entity/task'
 import mongoose from 'mongoose'
 
-export const Task = mongoose.model(
-  'Task',
-  new mongoose.Schema({
-    state: String,
-    description: String,
-    title: String
-  })
-)
+const taskModel = new mongoose.Schema<ITask>({
+  state: String,
+  description: String,
+  title: String
+})
+
+export const Task = mongoose.model('Task', taskModel)
